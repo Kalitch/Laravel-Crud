45b3d450014b6d667cf18fbfdb5f8e1075c5104b
WARMACHINE\Mk <marcelof730@gmail.com>
2020-11-14 18:02:31 -0300

    Start Commit

---
+++ database/migrations/2020_11_11_155253_Create_Product_Table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->float('price');
+            $table->integer('quantity');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
---
+++ resources/assets/js/components/products/ProductsCreate.vue
@@ -0,0 +1,74 @@
+
+<template>
+    <div>
+        <div class="form-group">
+            <router-link to="/" class="btn btn-default">Back</router-link>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Create new product</div>
+            <div class="panel-body">
+                <form v-on:submit="saveForm()">
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <label class="control-label">Product name</label>
+                            <input type="text" v-model="product.name" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <label class="control-label">Product code</label>
+                            <input type="text" v-model="product.code" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <label class="control-label">Product price</label>
+                            <input type="text" v-model="product.price" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <label class="control-label">Product quantity</label>
+                            <input type="text" v-model="product.quantity" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <button class="btn btn-success">Create</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                product: {
+                    name: '',
+                    code: '',
+                    price: '',
+                    quantity: '',
+                    status: 1,
+                }
+            }
+        },
+        methods: {
+            saveForm() {
+                var app = this;
+                var newProduct = app.product;
+                axios.post('/api/v1/products', newProduct)
+                    .then(function (resp) {
+                        app.$router.push({path: '/'});
+                    })
+                    .catch(function (resp) {
+                        alert("Could not create your product");
+                    });
+            }
+        }
+    }
+</script>
